d2d861d1a48e1d4b5161ec14d265208b32783381
Joseph Spedale <jspedale@gmail.com>
2016-12-08 21:15:59 -0500

    Changed api call. Displaying avatar and other info on page

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ $(document).ready(function() {
 
 var streamers = [
   "ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp",
-  "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"
-  ]
+  "storbeck", "habathcx", "brunofin", "RobotCaleb", "noobs2ninjas"
+];
 
 // add 'brunofin' and 'comster404' to array which are nonexistent accounts
 //and make sure account for this with placeholders of some sort.
@@ -11,34 +11,33 @@ var streamers = [
 //make a loop that goes through the streamers array and for each one,
 //getJSON to retrieve the proper values and place in the DOM.
 for (var i = 0; i < streamers.length; i++) {
-  $.getJSON("https://wind-bow.hyperdev.space/twitch-api/streams/" + streamers[i] + "?callback=?", function(json) {
-    if (json.stream === null) {
-      $('.results').append("<div class='streams'>" + streamers[i] + " is offline </div><br />");
-    }
-    else {
-      var streamUser = json.stream.channel.display_name;
-      var streamTitle = json.stream.channel.status;
-      var streamGame = json.stream.channel.game;
-      var streamLink = json.stream._links.self;
-      var title = $("<div class='streams'>" + streamTitle + "</div><br />");
-      var otherStuff = $("<div class='streams'>" + streamUser + " " + streamGame + "<br />"
-        + streamLink + "</div><br />")
-      $('.results').append(title);
-      $('.results').append(otherStuff);
-    }
-  })
+  $.getJSON("https://wind-bow.gomix.me/twitch-api/channels/" + streamers[i] + "?callback=?", function(json) {
+   console.log(streamers[i]);
+   if (json.status === null) {
+     $('.results').append("<div class='streambox'>" + json.display_name + " is offline </div><br />");
+   }
+   else if (json.display_name === undefined) {
+     $('.results').append("<div class='streambox'>" + streamers[i] + " user does not exist</div><br />");
+   }
+   else {
+     var streamUser = json.display_name;
+     var streamTitle = json.status;
+     var streamGame = json.game;
+     var streamLink = json.url;
+     var logo = json.logo;
+     var title = $("<div class='streambox'><div class='logo'><img src=" + logo +
+                    "></img></div><div class='streaminfo'><div class='streamuser'" + streamUser +
+                    "</div><div class='streamtitle'>" + streamTitle +
+                    "</div><div class='game'><a href=" + streamLink + ">" + streamGame + "</a></div></div></div>");
+     console.log(streamUser);
+     var otherStuff = $("<div class='streams'>" + streamUser + " " + streamGame + "<br />"
+       + streamLink + "</div><br />")
+     $('.results').append(title);
+
+    //  $('.streambox').append(otherStuff);
+   }
+  });
 }
-// sample API call:
-// $.getJSON("https://wind-bow.hyperdev.space/twitch-api/streams/esl_sc2?callback=?", function(json) {
-//   var streamUser = json.stream.channel.display_name;
-//   var streamTitle = json.stream.channel.status;
-//   var streamGame = json.stream.channel.game;
-//   var streamLink = json.stream._links.self;
-//
-//   console.log(streamUser + ": " + streamTitle);
-//   console.log(streamLink);
-//   console.log(json);
-// })
 
 
 
